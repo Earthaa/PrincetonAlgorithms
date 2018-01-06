202072e72d89d2c73834a0b7950d8b85f160e590
Earthaa <2892212@qq.com>
2018-01-06 17:17:23 +0800

    Week 6 update

---
+++ week6/wordnet/src/Outcast.java
@@ -0,0 +1,38 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+public class Outcast {
+    private WordNet myWordNet = null;
+    public Outcast(WordNet wordnet)
+    {
+        myWordNet = wordnet;
+    }// constructor takes a WordNet object
+    public String outcast(String[] nouns)
+    {
+        int[] distance = new int[nouns.length];
+        for(int i=0;i<nouns.length;i++)
+        {
+            for(int j = 0;j<nouns.length;j++)
+                distance[i]+=myWordNet.distance(nouns[i], nouns[j]);
+        }
+        int max = 0;
+        int flag = 0;
+        for(int i=0;i<nouns.length;i++)
+        {
+            if(distance[i] > max)
+            {
+                max = distance[i];
+                flag = i;
+            }
+        }
+        return nouns[flag];
+    }// given an array of WordNet nouns, return an outcast
+    public static void main(String[] args)
+    {
+        WordNet wordnet = new WordNet("synsets.txt", "hypernyms.txt");
+        Outcast outcast = new Outcast(wordnet);
+        In in = new In("outcast7.txt");
+        String[] nouns = in.readAllStrings();
+        StdOut.println(outcast.outcast(nouns));
+        
+    }// see test client below
+}
